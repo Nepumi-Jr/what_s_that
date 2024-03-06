3d3bfd82b062a9ceb72088462547dcf97a031cc0
Nepumi-Jr <peerator@kkumail.com>
2024-03-06 15:13:00 +0700

    fix scene

--- src/service/scene_nevigate.py
+++ src/service/scene_nevigate.py
@@ -1,32 +1,45 @@
 from src.scene import main_game, sync
-from src.scene.translator_main_game import syncing
+from src.scene import translator_main_game as translate_scene
 from src.service.scene import SCENE
+from src.service import config
+
 
 def main():
     # start with...
     cur_scene = SCENE.SYNC
 
-    while True:
-        if cur_scene == SCENE.MENU:
-            cur_scene = sync.main() # TODO:
-        elif cur_scene == SCENE.MAIN_GAME:
-            cur_scene = main_game.main()
-        elif cur_scene == SCENE.SYNC:
-            cur_scene = sync.main()
-        elif cur_scene == SCENE.CORRECT:
-            cur_scene = main_game.on_correct()
-        elif cur_scene == SCENE.WRONG:
-            cur_scene = main_game.on_wrong()
-        elif cur_scene == SCENE.WIN:
-            cur_scene = main_game.on_win()
-        elif cur_scene == SCENE.TIME_UP:
-            cur_scene = main_game.on_time_up()
-        
-        elif cur_scene == SCENE.TRANSLATOR_SYNC:
-            cur_scene = syncing()
+    conf = config.get_config()
+    if conf.device.mode == config.DeviceType.PLAY:
+        while True:
+            if cur_scene == SCENE.MENU:
+                cur_scene = sync.main() # TODO:
+            elif cur_scene == SCENE.MAIN_GAME:
+                cur_scene = main_game.main()
+            elif cur_scene == SCENE.SYNC:
+                cur_scene = sync.main()
+            elif cur_scene == SCENE.CORRECT:
+                cur_scene = main_game.on_correct()
+            elif cur_scene == SCENE.WRONG:
+                cur_scene = main_game.on_wrong()
+            elif cur_scene == SCENE.WIN:
+                cur_scene = main_game.on_win()
+            elif cur_scene == SCENE.TIME_UP:
+                cur_scene = main_game.on_time_up()
+            elif cur_scene == SCENE.EXIT:
+                break
 
-        elif cur_scene == SCENE.EXIT:
-            break
+            else:
+                raise ValueError(f"Invalid scene: {cur_scene}")
+    else:
+        while True:
+            if cur_scene == SCENE.MENU:
+                cur_scene = translate_scene.main() # TODO:
+            elif cur_scene == SCENE.TRANSLATOR_SYNC:
+                cur_scene = translate_scene.syncing()
+            elif cur_scene == SCENE.TRANSLATOR_MAIN_GAME:
+                cur_scene = translate_scene.translator_main_game()
+            elif cur_scene == SCENE.EXIT:
+                break
 
-        else:
-            raise ValueError(f"Invalid scene: {cur_scene}")
+            else:
+                raise ValueError(f"Invalid scene: {cur_scene}")
